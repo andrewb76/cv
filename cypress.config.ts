f0f97708f07bef873def6aa0b196de655cb698a2
Andrew Butov <andrewb@bk.ru>
2022-11-14 19:44:21 +0300

    debug cypress github action

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:8080',
+    defaultCommandTimeout: 10000,
     chromeWebSecurity: false,
     excludeSpecPattern: '*.{js|ts}',
     specPattern: 'cypress/**/*.feature',
