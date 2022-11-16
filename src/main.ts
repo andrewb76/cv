05e9e9c05d65cae1f00b6aa1f2a505248f54948c
Andrew Butov <andrewb@bk.ru>
2022-11-16 14:03:28 +0300

    debug issue with sentry

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './style.scss'
 import { createPinia } from 'pinia'
-import * as Sentry from '@sentry/vue'
-import { BrowserTracing } from '@sentry/tracing'
+// import * as Sentry from '@sentry/vue'
+// import { BrowserTracing } from '@sentry/tracing'
 import Hotjar from 'vue-hotjar'
 import i18n from './i18n'
 import { config } from '@/config'
@@ -14,20 +14,20 @@ const HOTJAR_ID = config.services.hotjar.id
 const pinia = createPinia()
 
 const app = createApp(App)
-Sentry.init({
-  app,
-  dsn: process.env.VUE_APP_SENTRY_DSN,
-  integrations: [
-    new BrowserTracing({
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-      tracingOrigins: ['localhost', 'butov.online', /^\//]
-    })
-  ],
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0
-})
+// Sentry.init({
+//   app,
+//   dsn: process.env.VUE_APP_SENTRY_DSN,
+//   integrations: [
+//     new BrowserTracing({
+//       routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+//       tracingOrigins: ['localhost', 'butov.online', /^\//]
+//     })
+//   ],
+//   // Set tracesSampleRate to 1.0 to capture 100%
+//   // of transactions for performance monitoring.
+//   // We recommend adjusting this value in production
+//   tracesSampleRate: 1.0
+// })
 
 app
   .use(i18n)
